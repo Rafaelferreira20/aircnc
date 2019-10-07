abd7e1f078b8120e7efde2c408f5a1b0e6e5a1ad
Rafael Ferreira <rafael.ferreira@before.com.br>
2019-10-06 22:12:25 -0400

    1hr

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const cors = require ('cors');
 
 const routes = require('./routes/routes');
 
@@ -9,7 +10,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-egwl1.mongodb.net/
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+app.use(cors());
+// app.use(cors({ origin: 'http://localhost:3333' }));
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import './App.css';
+
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
+  
+  
   return (
-    <div className="contaner">
+    <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       <div className="content">
-        <p>Ofereça <strong>Spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!</p>
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            id="email" 
-            type="email" 
-            placeholder="Seu email" 
-          />
-          <button type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
 }
-
 export default App;
